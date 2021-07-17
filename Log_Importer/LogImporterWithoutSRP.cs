5948ade43e6b754fac2c057277f1f1a7edd16232
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-07-17 20:56:25 +0300

    LogImporterWithoutSRP class is added.

---
+++ Log_Importer/LogImporterWithoutSRP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Log_Importer
+{
+    public class LogImporterWithoutSRP
+    {
+        public void ImportLogs()
+        {
+            string[] logFiles = GetLogFiles();
+            foreach (var logFile in logFiles)
+            {
+                var logEntries = ReadLogEntries();
+                ShowLogEntries(logEntries);
+            }
+        }
+
+        private void ShowLogEntries(IEnumerable<string> logEntries)
+        {
+            foreach(var entry in logEntries)
+            {
+                var logUnit = ParseLogEntry(entry);
+                Console.WriteLine(logUnit.ToString());
+            }
+        }
+
+        private object ParseLogEntry(string entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<string> ReadLogEntries()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string[] GetLogFiles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
