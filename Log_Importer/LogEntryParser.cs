cf9244f3a4b6f3f18406c8d52fe47f58ddbd61f2
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-07-17 23:18:28 +0300

    Add parsing for log entries.

---
+++ Log_Importer/LogEntryParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Log_Importer
+{
+    class LogEntryParser
+    {
+        public bool TryParse(string line, out LogEntry logEntry)
+        {
+            if (line is null)
+            {
+                logEntry = null;
+                return false;
+            }
+
+            int indexOfDate = line.IndexOf("Date: ", StringComparison.OrdinalIgnoreCase);
+            int indexOfSeverity = line.IndexOf("Severity: ", StringComparison.OrdinalIgnoreCase);
+            int indexOfMessage = line.IndexOf("Message: ", StringComparison.OrdinalIgnoreCase);
+            DateTime dateTime = DateTime.Parse(line[indexOfDate..indexOfSeverity]);
+            string severity = line[indexOfSeverity..indexOfMessage];
+            string message = line[indexOfMessage..];
+            logEntry = new LogEntry(dateTime, severity, message);
+
+            return true;
+        }
+    }
+}
